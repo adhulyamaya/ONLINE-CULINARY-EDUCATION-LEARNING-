4852178dc378f88c5869aa79d1978951458ed3be
adhulyamaya <adhulyamaya927@gmail.com>
2024-02-18 09:54:15 +0530

    latest just updating

--- frontend/src/components/Mycourses.jsx
+++ frontend/src/components/Mycourses.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axiosInstance from '../axios/axios';
 
-
 const MyCourses = () => {
-
   const [mycourse, setMycourses] = useState([]);
 
-
-
-  useEffect(()=>{
+  useEffect(() => {
     axiosInstance.get('purchased-courses/')
-    .then((res)=>{
-        console.log(res.data)
-        setMycourses(res.data)
-
-    })
-
-    
-    .catch((error)=>{
+      .then((res) => {
+        console.log(res.data);
+        setMycourses(res.data.userdata);
+      })
+      .catch((error) => {
         console.error(error);
-    });
-},[]);
-
+      });
+  }, []);
 
   return (
     <div>
       <div style={rectangularBoxStyle}>
-        
         <p>This is a rectangular box within MyCourses component.</p>
+        {mycourse.map((course, index) => (
+          <div key={index}>
+            <p>ID: {course.id}</p>
+            <p>Order Date: {course.order_date}</p>
+            <p>Order Time: {course.order_time}</p>
+            <p>Payment Amount: {course.payment_amount}</p>
+            <p>Payment Status: {course.payment_status}</p>
+            {/* Add more details as needed */}
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
 const rectangularBoxStyle = {
-  width: '500px',          
-  height: '500px',         
-  border: '2px solid #000', 
-  padding: '20px',        
-  margin: '20px',         
+  width: '500px',
+  height: '500px',
+  border: '2px solid #000',
+  padding: '20px',
+  margin: '20px',
 };
 
 export default MyCourses;
+
 
 
 
